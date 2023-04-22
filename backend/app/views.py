e246ac9a1dc43fdd2df1c016560dc199919bcf83
Hannah Willoughby <hannah@hannahwilloughby.dev>
2023-04-22 17:23:43 -0400

    Small updates

--- backend/app/views.py
+++ backend/app/views.py
@@ -5,5 +5,6 @@ from .models import Entry
 
 # Create your views here.
 class EntryView(viewsets.ModelViewSet):
+  http_method_names = ['get']
   serializer_class = EntrySerializer
   queryset = Entry.objects.all().order_by('-date')
