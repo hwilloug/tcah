14bd23a1bfbfdbc198fce6f4145a9bee943b63ff
Hannah Willoughby <hannah@hannahwilloughby.dev>
2023-04-22 16:58:14 -0400

    Refactor to use components

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,32 @@
+import Typography from '@mui/material/Typography'
+import Container from '@mui/material/Container'
+import FacebookIcon from '@mui/icons-material/Facebook'
+import List from '@mui/material/List'
+import ListItem from '@mui/material/ListItem'
+import Link from '@mui/material/Link'
+
+function Footer() {
+  return (
+    <Container
+      id='footer'
+      sx={{
+        padding: '10px',
+        mt: '10px',
+        backgroundColor: 'aliceblue'
+      }}
+    >
+      <List>
+        <ListItem><Typography sx={{fontWeight: '700'}}>
+          Tega Cay Animal Hospital
+        </Typography></ListItem>
+        <ListItem><Link href='/about' sx={{color: 'black'}}>About Us</Link></ListItem>
+        <ListItem><Link href='/book' sx={{color: 'black'}}>Book an Appointment</Link></ListItem>
+        <ListItem><Link href='/sitemap' sx={{color: 'black'}}>Sitemap</Link></ListItem>
+        <ListItem><FacebookIcon /></ListItem>
+        </List>
+      <Typography align='center'>© Willoughby Animal Hospitals, Inc</Typography>
+    </Container>
+  )
+}
+
+export default Footer
---
+++ frontend/src/components/LandingPage.js
@@ -0,0 +1,82 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import Typography from '@mui/material/Typography'
+import Container from '@mui/material/Container'
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import Image from 'mui-image'
+import List from '@mui/material/List'
+import ListItem from '@mui/material/ListItem'
+
+function LandingPage() {
+
+  const [entries, setEntries] = useState([])
+
+  const getEntries = async () => {
+    const response = await axios.get('/api/entry')
+    setEntries(response.data)
+  }
+
+  useEffect(() => {
+    getEntries()
+  }, [])
+
+  return (
+    <>
+      <Container disableGutters>
+        <Image 
+          src='dogs_running.jpg'
+        />
+        <Typography
+          variant='h4'
+          sx={{
+            fontWeight: 700,
+            color: 'white',
+            position: 'absolute',
+            top: '7rem',
+            left: '3rem',
+          }}
+        >
+          Compassionate Pet Care and Great Value since 1995!
+        </Typography>
+      </Container>
+      <Container
+        sx={{
+          display: 'flex'
+        }}
+      >
+        <Container sx={{mt:'20px'}}>
+          <Box>
+            <Typography variant='h6'>Hours:</Typography>
+            <List>
+              <ListItem>Monday-Thursday: 7 AM - 6 PM</ListItem>
+              <ListItem>Friday: 7 AM - 12 PM</ListItem>
+              <ListItem>Saturday-Sunday: closed</ListItem>
+            </List>
+            <Button
+              variant='contained'
+              size='large'
+            >Book an Appointment</Button>
+          </Box>
+        </Container>
+        <Container>
+          { entries.map((entry) => (
+            <Box
+              sx={{
+                border: '1px solid blue',
+                padding: '10px',
+                mt: '20px'
+              }}
+            >
+              <h2>{entry.title}</h2>
+              <p>{entry.date}</p>
+              <p>{entry.content}</p>
+            </Box>
+          )) }
+        </Container>
+      </Container>
+    </>
+  )
+}
+
+export default LandingPage
